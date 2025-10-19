b9ed3f18d7465158ef7c82bc0240989908dc430d
CulmoneY <yculmone@rogers.com>
2025-10-19 15:04:46 -0400

    [INGA] Required Login Through Session ID

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
+
   allow_browser versions: :modern
+  before_action :require_login
+
+  def require_login
+    redirect_to login_path, flash: { error: "You must be logged in to access this page" } unless logged_in?
+  end
 end
