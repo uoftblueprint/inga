01e342b69c85d65f2cf68295e6caeec78ddfcec5
CulmoneY <yculmone@rogers.com>
2025-10-19 15:04:46 -0400

    [INGA] Required Login Through Session ID

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def logged_in? = !!session[:user_id]
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if logged_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   # FizzBuzz routes
   get  "fizz_buzz", to: "fizz_buzz#index", as: "fizz_buzz"
   root "fizz_buzz#index"
+
+  # User routes
+  resources :users, only: [ :new, :create, :show ]
+
+  # Session routes
+  get "/login", to: "sessions#login"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
