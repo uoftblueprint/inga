5d4be23c38d563f9b754b2f1b44d7a3969cb5b70
CulmoneY <yculmone@rogers.com>
2025-10-20 20:59:37 -0400

    [INGA] Initalized Factory Bot And Created User Factory

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,12 +4,16 @@ require "rails/test_help"
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
+    include FactoryBot::Syntax::Methods
     parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
-    # Add more helper methods to be used by all tests here...
+    def create_logged_in_user
+      user = create(:user)
+      session[:user_id] = user.id
+      user
+    end
   end
 end
