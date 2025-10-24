4823363bdd4704e56ff5a3fbe20fd4d415df9d63
CulmoneY <yculmone@rogers.com>
2025-10-23 22:33:41 -0400

    [INGA] Populate db/seeds with inital users

---
+++ db/seeds/users.rb
@@ -0,0 +1,31 @@
+module Seeds
+  class Users
+    class << self
+      def run
+        ActiveRecord::Base.connection.truncate_tables('users', 'user_roles')
+        seed_users
+      end
+
+      private
+
+      def seed_users
+        puts "Seeding users..."
+        ActiveRecord::Base.transaction do
+          admin = User.create!(username: "admin", password: "a")
+          super_user = User.create!(username: "super", password: "a")
+          logger = User.create!(username: "logger", password: "a")
+          analyst = User.create!(username: "analyst", password: "a")
+
+          set_roles(admin, :admin)
+          set_roles(logger, :logger)
+          set_roles(analyst, :analyst)
+          set_roles(super_user, :admin, :logger, :analyst)
+        end
+      end
+
+      def set_roles(user, *roles)
+        roles.each { |role| UserRole.create!(user: user, role: role) }
+      end
+    end
+  end
+end
