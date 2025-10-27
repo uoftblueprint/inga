f1efcfffb7a977f27f9b6209b0080eb67a62d3bf
CulmoneY <yculmone@rogers.com>
2025-10-26 22:13:29 -0400

    [INGA] Added Reuseable Icon Component

---
+++ app/components/icon_component.rb
@@ -0,0 +1,19 @@
+class IconComponent < ViewComponent::Base
+  erb_template <<-ERB
+    <%= helpers.inline_svg_tag "icons/\#{@icon}.svg", size: size, class: @classes %>
+  ERB
+
+  SIZE_MAP = { xs: "12", sm: "16", md: "24", lg: "32", xl: "48" }.freeze
+
+  def initialize(icon:, size: :sm, classes: "")
+    @icon = icon
+    @size = size
+    @classes = classes
+  end
+
+  private
+
+  def size
+    SIZE_MAP[@size] || @size.to_s
+  end
+end
