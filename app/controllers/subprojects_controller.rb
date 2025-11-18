651c471770f7cc96685e7c6075ac6357281060c5
Harry Xu <harryxu497@gmail.com>
2025-11-17 23:53:20 -0500

    fix: fix or disable linter warnings

--- app/controllers/subprojects_controller.rb
+++ app/controllers/subprojects_controller.rb
@@ -9,10 +9,10 @@ class SubprojectsController < ApplicationController
     @subproject = @project.subprojects.build(subproject_params)
 
     if @subproject.save
-      flash[:success] = "Subproject created successfully."
+      flash[:success] = "Subproject created successfully." # rubocop:disable Rails/I18nLocaleTexts
       redirect_to new_project_subproject_path(@project)
     else
-      flash.now[:error] = "Failed to create subproject."
+      flash.now[:error] = "Failed to create subproject." # rubocop:disable Rails/I18nLocaleTexts
       render :new, status: :unprocessable_entity
     end
   end
