00e2b986139f1030cd6442c246f4b1994d430439
feng <alec.jiang@gmail.com>
2025-11-21 15:07:34 -0500

    feat: create ProjectController with create and new methods

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,25 @@
+class ProjectsController < ApplicationController
+    def new
+        @project = Project.new
+    end
+
+    def create
+        @project = Project.new(new_params)
+        @project.active = true
+
+        if @project.save
+            redirect_to(@project, flash: "Project was successfully created.")
+        else
+            flash.now[:error] = "Failed to create project."
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def new_params
+        params.require(:project).permit(:name, :description)
+    end
+
+    def has_required_roles? = true
+end
