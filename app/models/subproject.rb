6f694d5fe59e1dfcbf958f44e417d4e8794a1121
Harry Xu <harryxu497@gmail.com>
2025-11-20 22:28:01 -0500

    feat: make subproject address and description optional
    Remove subproject address and description model validations
    Make other fields required in the form view
    Removed unnecessary render of flash partial

--- app/models/subproject.rb
+++ app/models/subproject.rb
@@ -3,8 +3,6 @@ class Subproject < ApplicationRecord
   belongs_to :project
 
   validates :name, presence: true, uniqueness: { scope: :project_id }
-  validates :description, presence: true
-  validates :address, presence: true
   has_many :log_entries
   has_many :journals
 end
