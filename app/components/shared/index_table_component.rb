67ef18edde6cd49626a155cb37bbcde4fa312b17
Yianni Culmone <154041314+CulmoneY@users.noreply.github.com>
2025-11-24 19:05:39 -0500

    [INGA] Modified the UI for the IndexTableComponent

--- app/components/shared/index_table_component.rb
+++ app/components/shared/index_table_component.rb
@@ -1,26 +1,31 @@
 module Shared
   class IndexTableComponent < ViewComponent::Base
-    attr_reader :records
+    attr_reader :records, :columns
+
+    Column = Struct.new(:attribute, :header, :cell_renderer)
 
     def initialize(records:)
       super()
       @records = records
+      @columns = []
+    end
+
+    def column(attribute, header: nil, &cell_renderer)
+      @columns << Column.new(attribute, header || attribute.to_s.humanize, cell_renderer)
     end
 
     private
 
-    def headers
-      return [] if @records.empty?
-
-      @records.first.attributes.keys.map(&:humanize)
+    def render_cell_content(record, column)
+      if column.cell_renderer
+        view_context.capture(record, &column.cell_renderer)
+      else
+        record.send(column.attribute)
+      end
     end
 
-    def render_cell_content(value:)
-      if value.is_a? IndexTableComponentCell
-        render value
-      else
-        value.to_s
-      end
+    def before_render
+      content
     end
   end
 end
