235ae084b73b891f94708cec872994eea18f75a7
Ali K <alikhanpro60@gmail.com>
2025-11-29 21:33:15 -0500

    feat: add SubprojectsController show action with view, routing, and tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Project routes
   resources :projects, only: [] do
     # Subproject routes
-    resources :subprojects, only: %i[new create]
+    resources :subprojects, only: %i[new create show]
   end
 
   # Region routes
