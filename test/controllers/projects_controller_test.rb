ff40c46d0084393377765f04d5da3b57d5c5a552
feng <alec.jiang@gmail.com>
2025-12-03 22:09:09 -0500

    update projects controller testing

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -5,13 +5,54 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
     create_logged_in_admin_user
   end
 
-  test "create makes project with valid params" do
-    assert_difference("Project.count", 1) do
-      post projects_path, params: { project: { name: "Test Project", description: "Test description" } }
+  [
+    { route: "new", method: :get, url_helper: :new_project_url },
+    { route: "create", method: :post, url_helper: :projects_url }
+  ].each do |hash|
+    test "##{hash[:route]} redirects to login route when a user is not authenticated" do
+      log_out_user
+
+      public_send(hash[:method], public_send(hash[:url_helper]))
+      assert_response :redirect
+      assert_redirected_to login_path
     end
 
-    assert_redirected_to project_path(Project.last)
-    assert_equal true, Project.last.active
+    test "##{hash[:route]} redirects to root route when a user is not authorized" do
+      create_logged_in_user
+
+      public_send(hash[:method], public_send(hash[:url_helper]))
+      assert_response :redirect
+      assert_redirected_to root_path
+    end
+  end
+
+  test "#new renders successfully when a user is an admin" do
+    get new_project_url
+    assert_response :success
+  end
+
+  test "create makes project with valid params" do
+    project_name = "Test Project"
+
+    assert_difference("Project.count", 1) do
+      post projects_path, params: { project: { name: project_name, description: "Test description", active: true } }
+    end
+
+    new_project = Project.find_by!(name: project_name)
+    assert_equal "Test description", new_project.description
+    assert new_project.active
+  end
+
+  test "create makes inactive project when active is false" do
+    project_name = "Inactive Project"
+
+    assert_difference("Project.count", 1) do
+      post projects_path, params: { project: { name: project_name, description: "Test description", active: false } }
+    end
+
+    new_project = Project.find_by!(name: project_name)
+    assert_equal "Test description", new_project.description
+    assert_not new_project.active
   end
 
   test "create does not create project without name" do
@@ -20,20 +61,5 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_response :unprocessable_entity
-  end
-
-  test "create does not create project without description" do
-    assert_no_difference("Project.count") do
-      post projects_path, params: { project: { name: "Test Project", description: "" } }
-    end
-
-    assert_response :unprocessable_entity
-  end
-
-  test "create sets project as active by default" do
-    post projects_path, params: { project: { name: "Test Project", description: "Test description" } }
-
-    project = Project.last
-    assert project.active
   end
 end
