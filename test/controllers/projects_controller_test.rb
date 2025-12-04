3341f5d101db55b9b9bb17d3bea0a764202fbf60
20luky3 <20luky301@gmail.com>
2025-12-03 22:38:52 -0500

    Refactor show project tests for reusability

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -3,9 +3,35 @@ require "test_helper"
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
   setup do
     create_logged_in_admin_user
+    @project = create(:project)
   end
 
-  test "#show renders successfully when user is authorised" do
+  [
+    { route: "show", method: :get, url_helper: :project_url }
+  ].each do |hash|
+    test "##{hash[:route]} redirects to login route when a user is not authenticated" do
+      log_out_user
+
+      public_send(hash[:method], public_send(hash[:url_helper], @project))
+      assert_response :redirect
+      assert_redirected_to login_path
+    end
+
+    test "##{hash[:route]} redirects to root route when a user is not authorized" do
+      create_logged_in_user
+
+      public_send(hash[:method], public_send(hash[:url_helper], @project))
+      assert_response :redirect
+      assert_redirected_to root_path
+    end
+
+    test "##{hash[:route]} renders successfully when a user is an admin" do
+      public_send(hash[:method], public_send(hash[:url_helper], @project))
+      assert_response :success
+    end
+  end
+
+  test "#show renders the project data correctly" do
     project = create(:project,
                      name: "Test project",
                      description: "Project description",
@@ -16,15 +42,5 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
 
     assert_match project.name, response.body
     assert_match project.description, response.body
-    assert_match "Yes", response.body
-  end
-
-  test "#show redirects to login route when user is not logged in" do
-    log_out_user
-
-    project = create(:project)
-    get project_path(project)
-    assert_response :redirect
-    assert_redirected_to login_path
   end
 end
