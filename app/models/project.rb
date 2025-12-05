bdc83d35c721b1b22a9ce45336c2c121c5770e5f
feng <113765376+hari-co@users.noreply.github.com>
2025-12-04 22:46:45 -0500

    [PROJECTS] Added Unqiue Index On Projects (#157)

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,7 @@ class Project < ApplicationRecord
   has_many :subprojects
   has_many :reports
 
+  validates :name, presence: true, uniqueness: true
+
   scope :active, -> { where(active: true) }
 end
