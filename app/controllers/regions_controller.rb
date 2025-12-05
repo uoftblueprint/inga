c040f2065ebc99f16c9c232bbd7eccc9050664aa
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2025-12-04 23:10:20 -0500

    feat(regions): controller action for create

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -3,6 +3,26 @@ class RegionsController < ApplicationController
     @regions = Region.all
   end
 
+  def create
+    @region = Region.new(region_params)
+
+    if @region.save
+      redirect_to(
+        regions_path,
+        flash: { success: t(".success") }
+      )
+    else
+      flash.now[:error] = t(".error")
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def region_params
+    params.expect(region: %i[name latitude longitude])
+  end
+
   def has_required_roles?
     current_user.has_roles?(:admin)
   end
