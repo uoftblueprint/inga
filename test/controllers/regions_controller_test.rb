bc3b68ff4436721b8dafc89171c3e82ffde4e6d6
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2025-12-04 23:52:08 -0500

    test(regions): controller tests for index, new, and create

--- test/controllers/regions_controller_test.rb
+++ test/controllers/regions_controller_test.rb
@@ -2,32 +2,91 @@ require "test_helper"
 
 class RegionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @region = create(:region, name: "region1")
     create_logged_in_admin_user
+    @region = create(:region, name: "region1")
   end
 
-  test "#index redirects to login path when user is not logged in" do
-    log_out_user
-    get regions_path
-    assert_redirected_to login_path
+  [
+    { route: "index", method: :get, url_helper: :regions_url  },
+    { route: "new", method: :get, url_helper: :new_region_url },
+    { route: "create", method: :post, url_helper: :regions_url }
+  ].each do |hash|
+    test "##{hash[:route]} redirects to login route when a user is not authenticated" do
+      log_out_user
+
+      public_send(hash[:method], public_send(hash[:url_helper]))
+      assert_response :redirect
+      assert_redirected_to login_path
+    end
+
+    test "##{hash[:route]} redirects to root route when a user is not authorized" do
+      create_logged_in_user
+
+      public_send(hash[:method], public_send(hash[:url_helper]))
+      assert_response :redirect
+      assert_redirected_to root_path
+    end
   end
 
-  test "#index successfully renders when user is an admin" do
-    get regions_path
-    assert_response :success
-  end
-
-  test "#index redirects to root path when user is not authorized" do
-    create_logged_in_user
-    get regions_path
-    assert_redirected_to root_path
+  [
+    { route: "index", method: :get, url_helper: :regions_url },
+    { route: "new", method: :get, url_helper: :new_region_url }
+  ].each do |hash|
+    test "##{hash[:route]} renders successfully when a user is an admin" do
+      public_send(hash[:method], public_send(hash[:url_helper]))
+      assert_response :success
+    end
   end
 
   test "#index displays all regions" do
-    create(:region, name: "region2")
+    region2 = create(:region, name: "region2")
+
     get regions_path
+
     assert_response :success
-    assert_select "td", text: "region1"
-    assert_select "td", text: "region2"
+    assert_select "td", text: @region.name
+    assert_select "td", text: region2.name
+  end
+
+  test "#create successfully creates a region with valid params" do
+    region2 = build(:region, name: "region2")
+
+    region_params = {
+      name: region2.name,
+      latitude: region2.latitude,
+      longitude: region2.longitude
+    }
+
+    assert_difference("Region.count", 1) do
+      post regions_path, params: {
+        region: region_params
+      }
+    end
+
+    new_region = Region.find_by!(name: region2.name)
+    assert_equal region2.name, new_region.name
+  end
+
+  test "#create creates only one region with a given name" do
+    region2 = build(:region, name: "region2")
+
+    params = { region: {
+      name: region2.name,
+      latitude: region2.latitude,
+      longitude: region2.longitude
+    } }
+
+    assert_difference("Region.count", 1) do
+      post regions_path, params: params
+      post regions_path, params: params
+    end
+  end
+
+  test "#create does not create a region with invalid params" do
+    assert_no_difference("Region.count") do
+      post regions_path, params: { region: { name: "" } }
+    end
+
+    assert_response :unprocessable_entity
   end
 end
