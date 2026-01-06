ba22fbcc682f3e04c5a572e2f70ba2b4ab93da48
20luky3 <20luky301@gmail.com>
2026-01-05 21:48:21 -0500

    Change project model in order to delete dependent subprojects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,6 @@
 class Project < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-
-  has_many :subprojects
+  has_many :subprojects, dependent: :destroy
   has_many :reports
 
   validates :name, presence: true, uniqueness: true
