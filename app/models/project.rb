814c43c4d31984bc755015ea263a8cdf049a2989
20luky3 <20luky301@gmail.com>
2026-01-07 20:08:24 -0500

    Change delete behaviour if subprojects exist

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  has_many :subprojects, dependent: :destroy
+  has_many :subprojects, dependent: :restrict_with_error
   has_many :reports
 
   validates :name, presence: true, uniqueness: true
