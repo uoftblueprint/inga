78e684a202c87d8ab3a574cc967e2ba8a299ee97
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2026-01-26 00:24:18 -0500

    index_table_component: derive header from model

--- app/components/shared/index_table_component.rb
+++ app/components/shared/index_table_component.rb
@@ -12,7 +12,8 @@ module Shared
     end
 
     def column(attribute, header: nil, col_size: nil, &cell_renderer)
-      @columns << Column.new(attribute, header || attribute.to_s.humanize, cell_renderer, col_size)
+      label = header || default_header_for(attribute)
+      @columns << Column.new(attribute, label, cell_renderer, col_size)
     end
 
     private
@@ -22,6 +23,27 @@ module Shared
         view_context.capture(record, &column.cell_renderer)
       else
         record.send(column.attribute)
+      end
+    end
+
+    def model_class
+      # memoize to only have to compute value once per table
+      return @model_class if instance_variable_defined?(:@model_class)
+
+      @model_class = if records.respond_to?(:model) # for models
+        records.model
+      elsif records.respond_to?(:klass) # for relations
+        records.klass
+      end
+    end
+
+    def default_header_for(attribute)
+      klass = model_class
+
+      if klass && klass.respond_to?(:human_attribute_name)
+        klass.human_attribute_name(attribute)
+      else
+        attribute.to_s.humanize
       end
     end
 
