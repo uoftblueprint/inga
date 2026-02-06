ac0313cbefb3c97595bd99d4f16d764edfbb77f3
CulmoneY <yculmone@rogers.com>
2026-02-06 12:48:54 -0500

    [Seeds] Journal Seeds Now Render Markdown_Content as HTML

--- db/seeds/journals.rb
+++ db/seeds/journals.rb
@@ -1,3 +1,5 @@
+require "redcarpet"
+
 module Seeds
   class Journals
     class << self
@@ -12,12 +14,15 @@ module Seeds
         puts "Seeding journals..."
         return if user.nil? || subprojects.empty?
 
+        renderer = Redcarpet::Render::HTML.new(prettify: true)
+        markdown = Redcarpet::Markdown.new(renderer)
+
         subprojects.cycle(2) do |subproject|
           Journal.create!(
             user: user,
             subproject: subproject,
             title: Faker::Lorem.sentence,
-            markdown_content: Faker::Markdown.sandwich(sentences: 6, repeat: 3)
+            markdown_content: markdown.render(Faker::Markdown.sandwich(sentences: 6, repeat: 3))
           )
         end
       end
