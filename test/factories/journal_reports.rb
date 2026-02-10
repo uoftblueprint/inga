fd82d76fc90157eba9e39768432f62fa74861457
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2026-02-09 20:19:58 -0500

    journals_reports: add tests for associations

---
+++ test/factories/journal_reports.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :journal_report do
+    journal
+    report
+  end
+end
--- test/models/journal_test.rb
+++ test/models/journal_test.rb
@@ -1,4 +1,17 @@
 require "test_helper"
 
 class JournalTest < ActiveSupport::TestCase
+  def setup
+    @journal = create(:journal)
+  end
+
+  test "can have many reports associated with it" do
+    report1 = create(:report)
+    report2 = create(:report)
+
+    @journal.reports << report1
+    @journal.reports << report2
+
+    assert_equal 2, @journal.reports.count
+  end
 end
