dbb4a4c28bef8549939f6c5945c4e8ef7ff13af5
Harry Xu <harryxu497@gmail.com>
2026-02-10 19:29:08 -0500

    feat: add spanish language locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,22 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   before_action :require_login
   before_action :check_required_roles
+  before_action :set_locale
 
   private
+
+  def set_locale
+    requested_locale = params[:locale]&.to_sym
+    I18n.locale = if requested_locale && I18n.available_locales.include?(requested_locale)
+                    requested_locale
+                  else
+                    I18n.default_locale
+                  end
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   def require_login
     redirect_to login_path, flash: { error: "You must be logged in to access this page" } unless logged_in?
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,32 @@
 Rails.application.routes.draw do
-  # Project routes
-  resources :projects do
-    resource :attributes_schema, controller: "project_attributes", only: %i[update edit] do
-      get :new_row
-    end
-    # Subproject routes
-    resources :subprojects, except: [:index] do
-      resources :journals, except: [:index]
-      resources :log_entries, except: [:index]
-    end
-  end
-
-  # Region routes
-  resources :regions, except: [:show]
-
   # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root to: "projects#index"
+  # Locale scope
+  scope "(:locale)", locale: /#{I18n.available_locales.join('|')}/ do
+    # Project routes
+    resources :projects do
+      resource :attributes_schema, controller: "project_attributes", only: %i[update edit] do
+        get :new_row
+      end
+      # Subproject routes
+      resources :subprojects, except: [:index] do
+        resources :journals, except: [:index]
+        resources :log_entries, except: [:index]
+      end
+    end
 
-  # User routes
-  resources :users
+    # Region routes
+    resources :regions, except: [:show]
 
-  # Session routes
-  get "/login", to: "sessions#login"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+    root to: "projects#index"
+
+    # User routes
+    resources :users
+
+    # Session routes
+    get "/login", to: "sessions#login"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+  end
 end
