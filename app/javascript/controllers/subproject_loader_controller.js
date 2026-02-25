fb30b3948fe93f53d15d8ac4ca2a9dbc3df331c9
Rashu <shardarashu@gmail.com>
2026-02-24 22:26:06 -0500

    Lint fixes.

--- app/javascript/controllers/subproject_loader_controller.js
+++ app/javascript/controllers/subproject_loader_controller.js
@@ -1,41 +1,44 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["projectSelect", "subprojectSelect", "journalFields"]
+  static targets = ["projectSelect", "subprojectSelect", "journalFields"];
 
   load() {
-    const projectId = this.projectSelectTarget.value
+    const projectId = this.projectSelectTarget.value;
     if (!projectId) {
-      this.journalFieldsTarget.innerHTML = ""
-      return
+      this.journalFieldsTarget.innerHTML = "";
+      return;
     }
 
     fetch(`/projects/${projectId}/load_subprojects`, {
-      headers: { Accept: "text/vnd.turbo-stream.html" }
+      headers: { Accept: "text/vnd.turbo-stream.html" },
     })
-      .then(r => r.text())
-      .then(html => Turbo.renderStreamMessage(html))
+      .then((r) => r.text())
+      .then((html) => Turbo.renderStreamMessage(html));
   }
 
   subprojectSelectTargetConnected(element) {
     if (element.options.length > 1) {
-      element.disabled = false
+      element.disabled = false;
     }
 
     element.addEventListener("change", () => {
-      this.loadForm()
-    })
+      this.loadForm();
+    });
   }
 
   loadForm() {
-    const projectId = this.projectSelectTarget.value
-    const subprojectId = this.subprojectSelectTarget.value
-    if (!projectId || !subprojectId) return
+    const projectId = this.projectSelectTarget.value;
+    const subprojectId = this.subprojectSelectTarget.value;
+    if (!projectId || !subprojectId) return;
 
-    fetch(`/projects/${projectId}/load_journal_form?subproject_id=${subprojectId}`, {
-      headers: { Accept: "text/vnd.turbo-stream.html" }
-    })
-      .then(r => r.text())
-      .then(html => Turbo.renderStreamMessage(html))
+    fetch(
+      `/projects/${projectId}/load_journal_form?subproject_id=${subprojectId}`,
+      {
+        headers: { Accept: "text/vnd.turbo-stream.html" },
+      }
+    )
+      .then((r) => r.text())
+      .then((html) => Turbo.renderStreamMessage(html));
   }
 }
