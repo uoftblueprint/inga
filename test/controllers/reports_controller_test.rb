9219d81169e623ae6e5f4689a995309fe765d9de
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2026-03-16 22:22:13 -0400

    reports/edit: controller tests

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -9,7 +9,8 @@ class ReportsControllerTest < ActionDispatch::IntegrationTest
     { route: "show", method: :get, url_helper: :report_url, needs_report: true },
     { route: "new", method: :get, url_helper: :new_report_url, needs_report: false },
     { route: "edit", method: :get, url_helper: :edit_report_url, needs_report: true },
-    { route: "filter", method: :get, url_helper: :filter_reports_url, needs_report: false }
+    { route: "filter", method: :get, url_helper: :filter_reports_url, needs_report: false },
+    { route: "update", method: :patch, url_helper: :report_url, needs_report: true }
   ].each do |hash|
     test "##{hash[:route]} redirects to login route when a user is not authenticated" do
       log_out_user
@@ -30,7 +31,13 @@ class ReportsControllerTest < ActionDispatch::IntegrationTest
       assert_response :redirect
       assert_redirected_to root_path
     end
+  end
 
+  [
+    { route: "show", method: :get, url_helper: :report_url, needs_report: true },
+    { route: "new", method: :get, url_helper: :new_report_url, needs_report: false },
+    { route: "edit", method: :get, url_helper: :edit_report_url, needs_report: true }
+  ].each do |hash|
     test "##{hash[:route]} renders successfully when a user is an admin" do
       args = create(:report) if hash[:needs_report]
 
@@ -159,5 +166,64 @@ class ReportsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to new_report_path
     assert_equal I18n.t("reports.create.invalid"), flash[:error]
+  end
+
+  test "#update replaces journals and aggregated data in one save" do
+    report = create(:report)
+    existing_journal = create(:journal)
+    removed_journal = create(:journal)
+    added_journal = create(:journal)
+
+    report.journals << [existing_journal, removed_journal]
+
+    retained_datum = create(:aggregated_datum, report: report, additional_text: "Keep me", value: 10)
+    removed_datum = create(:aggregated_datum, report: report, additional_text: "Remove me", value: 5)
+
+    patch report_path(report), params: {
+      journal_ids: [existing_journal.id, added_journal.id],
+      retained_aggregated_datum_ids: [retained_datum.id],
+      new_aggregated_data: {
+        "0" => { value: "12.50", additional_text: "New entry" }
+      }
+    }
+
+    assert_response :redirect
+    assert_redirected_to report_path(report)
+    assert_equal I18n.t("reports.update.success"), flash[:success]
+
+    report.reload
+
+    assert_equal [existing_journal.id, added_journal.id].sort, report.journal_ids.sort
+    assert_includes report.aggregated_data.ids, retained_datum.id
+    assert_not_includes report.aggregated_data.ids, removed_datum.id
+
+    created = report.aggregated_data.find_by(additional_text: "New entry")
+
+    assert_not_nil created
+    assert_equal "AggregatedNumericalDatum", created.type
+    assert_equal 12.5, created.value.to_f
+  end
+
+  test "#update rejects malformed aggregated data" do
+    report = create(:report)
+    report_journal = create(:journal)
+    report.journals << report_journal
+    retained_datum = create(:aggregated_datum, report: report, additional_text: "Retain", value: 3)
+
+    patch report_path(report), params: {
+      journal_ids: [report_journal.id],
+      retained_aggregated_datum_ids: [retained_datum.id],
+      new_aggregated_data: {
+        "0" => { value: "", additional_text: "Missing value" }
+      }
+    }
+
+    assert_response :redirect
+    assert_redirected_to edit_report_path(report)
+    assert_equal I18n.t("reports.update.invalid"), flash[:error]
+
+    report.reload
+    assert_equal [report_journal.id], report.journal_ids
+    assert_equal [retained_datum.id], report.aggregated_data.ids
   end
 end
