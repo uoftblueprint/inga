908d44d8911cead731a2930f4c3a665f86ddccfd
Ali <alikhanpro60@gmail.com>
2026-03-20 22:53:06 -0400

    fix(sidebar): add reports navigation item and locale keys

--- app/components/sidebar_component.rb
+++ app/components/sidebar_component.rb
@@ -18,6 +18,8 @@ class SidebarComponent < ViewComponent::Base
       items: [
         SIDEBAR_ITEM.new(name: t("projects", scope: "components.sidebar_component.build_admin_section"),
                          path: helpers.projects_path, icon: "journal-text"),
+        SIDEBAR_ITEM.new(name: t("reports", scope: "components.sidebar_component.build_admin_section"),
+                         path: helpers.reports_path, icon: "reports-fill"),
         SIDEBAR_ITEM.new(name: t("new_report", scope: "components.sidebar_component.build_admin_section"),
                          path: helpers.new_report_path, icon: "add"),
         SIDEBAR_ITEM.new(name: t("users", scope: "components.sidebar_component.build_admin_section"),
