1d7984f18a801ae777d585b1bde1f7faaa5c9f1a
Ali K <alikhanpro60@gmail.com>
2026-03-26 23:21:29 -0400

    Added reports navigation Item in sidebar (#265)
    * feat: added controller, views, tests for reports
    
    * feat: add reports page to navigation
    
    * fix: added copilot suggestions
    
    * fix: remove changes from conflict resolution

--- app/components/sidebar_component.rb
+++ app/components/sidebar_component.rb
@@ -20,6 +20,8 @@ class SidebarComponent < ViewComponent::Base
                          path: helpers.projects_path, icon: "journal-text"),
         SIDEBAR_ITEM.new(name: t("new_report", scope: "components.sidebar_component.build_admin_section"),
                          path: helpers.new_report_path, icon: "add"),
+        SIDEBAR_ITEM.new(name: t("reports", scope: "components.sidebar_component.build_admin_section"),
+                         path: helpers.reports_path, icon: "reports-fill"),
         SIDEBAR_ITEM.new(name: t("users", scope: "components.sidebar_component.build_admin_section"),
                          path: helpers.users_path, icon: "person-gear"),
         SIDEBAR_ITEM.new(name: t("regions", scope: "components.sidebar_component.build_admin_section"),
