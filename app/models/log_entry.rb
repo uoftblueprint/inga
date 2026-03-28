2affc2d5073b149f5fd087145ec1acd00148b9af
Rashu <shardarashu@gmail.com>
2026-03-27 22:52:37 -0400

    Add i18n comment to LogEntry validator
    Insert an i18n-tasks-use comment into LogEntry#not_empty referencing activerecord.errors.models.log_entry.attributes.base.not_empty so i18n-tasks will detect the translation key. This is a no-op change at runtime and prevents the key from being reported as unused by static analysis.

--- app/models/log_entry.rb
+++ app/models/log_entry.rb
@@ -9,6 +9,7 @@ class LogEntry < ApplicationRecord
   def not_empty
     return if metadata.is_a?(Hash) && metadata.values.any?(&:present?)
 
+    # i18n-tasks-use t('activerecord.errors.models.log_entry.attributes.base.not_empty')
     errors.add(:base, :not_empty)
   end
 end
