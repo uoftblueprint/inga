60f3a455c44b1a0aa863ff90dcf21e4c7259e5f4
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2026-03-28 01:30:10 -0400

    modal: fix colours and add animations

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,14 +1,40 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["panel"];
+
   connect() {
-    this.element.classList.remove("hidden");
+    requestAnimationFrame(() => {
+      this.element.classList.remove("opacity-0");
+      if (!this.hasPanelTarget) return;
+
+      this.panelTarget.style.setProperty("--ui-enter-duration", "320ms");
+      this.panelTarget.style.setProperty("--ui-enter-from-y", "12px");
+      this.panelTarget.style.setProperty("--ui-enter-from-scale", "0.99");
+
+      this.panelTarget.classList.remove("ui-enter-item");
+      void this.panelTarget.offsetWidth;
+      this.panelTarget.classList.add("ui-enter-item");
+    });
   }
 
   close(event) {
     if (event?.detail?.success === false) return;
 
-    this.element.classList.add("hidden");
-    this.element.innerHTML = "";
+    if (!this.hasPanelTarget) {
+      this.element.remove();
+      return;
+    }
+
+    this.element.classList.add("opacity-0");
+    this.panelTarget.classList.add(
+      "opacity-0",
+      "translate-y-3",
+      "scale-[0.99]",
+    );
+
+    setTimeout(() => {
+      this.element.remove();
+    }, 220);
   }
 }
