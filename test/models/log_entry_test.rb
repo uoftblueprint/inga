10c80df2f1b864f9f80b6df870908dc3e622fd05
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2026-03-27 23:36:48 -0400

    log entry: add test for model not empty and fix z-index

--- test/models/log_entry_test.rb
+++ test/models/log_entry_test.rb
@@ -1,4 +1,11 @@
 require "test_helper"
 
 class LogEntryTest < ActiveSupport::TestCase
+  test "log entry is not valid if metadata is empty" do
+    log_entry = build(:log_entry, metadata: {})
+
+    assert_not log_entry.valid?
+    assert_includes log_entry.errors.full_messages,
+                    I18n.t("activerecord.errors.models.log_entry.attributes.base.not_empty")
+  end
 end
